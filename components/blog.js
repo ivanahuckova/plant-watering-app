468b4bfcab92202a0543ae826a80a19a65756396
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-06 17:17:59 +0100

    feat: Create hiding of text in blogpost after clicking the button

--- components/blog.js
+++ components/blog.js
@@ -2,12 +2,13 @@ class Blog {
 	constructor(blog) {
 		this.blog = blog;
 		this.button = blog.querySelector(".expand-button");
+		this.post = blog.querySelector(".blog-text");
 		this.button.addEventListener("click", () => {
 			this.expandBlog();
 		});
 	}
 	expandBlog() {
-		this.blog.classList.toggle("blog-open");
+		this.post.classList.toggle("text-display-none");
 	}
 }
 const blogs = document.querySelectorAll(".blog-post");
