357cb8e23064b13801c19adc5fe1fac3217077df
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-07 10:52:40 +0100

    feat: Add index.html to package.json to allow deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.use("/", express.static("public/index.html"));
+app.use("/", express.static("index.html"));
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log("App listening on port 3000!");
